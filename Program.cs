5322667b3ccf74d99f3ba9f450d8fe562377af15
omtriteju <96515845+omtriteju@users.noreply.github.com>
2024-10-04 17:29:03 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,32 +4,37 @@ using Groko.core.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configure Kestrel to listen on port 5000 on all IP addresses
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5000); // Listen on port 5000
+});
+
+// Add services to the container
 builder.Services.AddScoped<IEventRepository, EventRepository>();
 builder.Services.AddScoped<IEventService, EventService>();
 builder.Services.AddScoped<IBlogBytesService, BlogBytesService>();
 builder.Services.AddScoped<IBlogBytesRepository, BlogBytesRepository>();
 
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Configuration for MySQL Connection String
+// Configuration for MySQL Connection String from appsettings.json
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
 // Add CORS services
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigin",
-        builder => builder.WithOrigins("http://localhost:3000")
-                          .AllowAnyHeader()
-                          .AllowAnyMethod());
+        corsBuilder => corsBuilder.WithOrigins("http://localhost:3000")
+                                  .AllowAnyHeader()
+                                  .AllowAnyMethod());
 });
-
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -37,10 +42,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
 
 // Use CORS policy
 app.UseCors("AllowSpecificOrigin");
-app.Run();
 
+app.UseAuthorization();
+
+// Set up API routes
+app.MapControllers();
+
+app.Run();
